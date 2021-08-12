a8f971ea50b3ce8f35452a41c9e7079bfcc373e9
caiomcs04 <caio6c@yahoo.com.br>
2021-08-12 13:03:44 -0300

    2#Commit(Routes implementations)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import './assets/css/base/base.css'
+import React from "react";
+import "./assets/css/base/base.css";
+import Home from "./paginas/Home";
+import About from "./paginas/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-  <>
-
-  </>
-  )
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
