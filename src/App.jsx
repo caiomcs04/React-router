738f4ef10e7ae6e950ba8ee506f356e6a502f169
caiomcs04 <caio6c@yahoo.com.br>
2021-08-12 13:45:54 -0300

    4#Commit(Header implementation)

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Home from "./paginas/Home";
 import About from "./paginas/About";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Error404 from "./paginas/Error404";
+import Header from "./componentes/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Switch>
           <Route exact path="/">
             <Home />
